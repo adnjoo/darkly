ab2999ff777cdf50e1fb057d5b08efe0ee58f5cd
Andrew Njoo <adnjoo@gmail.com>
2024-09-23 09:17:53 -0500

    add files

---
+++ darkly/darklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  darklyApp.swift
+//  darkly
+//
+//  Created by Andrew Njoo on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct darklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
